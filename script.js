6074d0ddef3e1952ff0059933beb04c6cb418b94
Gabriel Sotero <gabriel.sotero08@gmail.com>
2023-01-11 11:31:34 -0400

    Botão para adivinhar implementado e sombra atrás do quadro negro adicionada

--- script.js
+++ script.js
@@ -6,30 +6,39 @@ console.log('Numero sorteado: ' + numSorteado);
 let campoNumero = document.querySelector('#numDigitado');
 campoNumero.addEventListener('keydown',responderEnter);
 
+let botao = document.querySelector('#botaoAdivinhar');
+botao.addEventListener('click',adivinharNumero);
+
 function responderEnter(event){
     if(event.key == 'Enter'){
-        let numDigitado = document.querySelector('#numDigitado').value;
-        document.querySelector('#aviso').style.visibility = 'visible';
-        if(numDigitado == numSorteado){
-            document.querySelector('#aviso').textContent = 'ACERTOUU!!!';
+        botao.click();
+    }
+}
+
+function adivinharNumero(event){
+    let numDigitado = document.querySelector('#numDigitado').value;
+    campoNumero.value = '';
+    document.querySelector('#aviso').style.visibility = 'visible';
+    if(numDigitado == numSorteado){
+        document.querySelector('#aviso').textContent = 'ACERTOUU!!!';
+        document.querySelector('#numeroTentativas').style.visibility = 'hidden';
+    }
+    else{
+        numTentativas--;
+        if(numTentativas == 0){
+            document.querySelector('#aviso').textContent = 'VOCÊ PERDEU O JOGO :(';
             document.querySelector('#numeroTentativas').style.visibility = 'hidden';
+            
+        }
+        else if(numDigitado > numSorteado){
+            document.querySelector('#aviso').textContent = 'NUMERO SORTEADO MENOR!!!';
+            document.querySelector('#numeroTentativas').textContent = 'Você tem ' + numTentativas + ' tentativas';
+                
         }
         else{
-            numTentativas--;
-            if(numTentativas == 0){
-                document.querySelector('#aviso').textContent = 'VOCÊ PERDEU O JOGO :(';
-                document.querySelector('#numeroTentativas').style.visibility = 'hidden';
-                
-            }
-            else if(numDigitado > numSorteado){
-                document.querySelector('#aviso').textContent = 'NUMERO SORTEADO MENOR!!!';
-                document.querySelector('#numeroTentativas').textContent = 'Você tem ' + numTentativas + ' tentativas';
-                   
-            }
-            else{
-                document.querySelector('#aviso').textContent = 'NUMERO SORTEADO MAIOR!!!';
-                document.querySelector('#numeroTentativas').textContent = 'Você tem ' + numTentativas + ' tentativas';
-            }
-        }    
-    }
+            document.querySelector('#aviso').textContent = 'NUMERO SORTEADO MAIOR!!!';
+            document.querySelector('#numeroTentativas').textContent = 'Você tem ' + numTentativas + ' tentativas';
+        }
+    }    
+
 }
